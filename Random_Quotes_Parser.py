37363883819553d841972acf5fab26681bfbf012
Dion Boles <dion.boles@gmail.com>
2019-01-09 14:54:48 -0500

    Create Random_Quotes_Parser.py

---
+++ Random_Quotes_Parser.py
@@ -0,0 +1,23 @@
+import requests,os,re
+from bs4 import BeautifulSoup
+
+if (os.path.exists("data.html") != True ):
+    page = requests.get("https://www.brainyquote.com/topics/random").text
+    open("data.html","w").write(page);
+    print("[+] Downloafding");
+
+
+page = open("data.html","r").read()
+soup = BeautifulSoup(page,'html.parser')
+
+a = soup.find_all('a')
+
+for i in a:
+    try:
+       if re.search(r'\b(?:title="view quote")',str(i)) is not None:
+            objString = '@"{}"\n'.format(i.text);
+            print(objString)
+            open("object_String.txt","a").write(objString)
+            print('')
+    except AttributeError:
+        pass
